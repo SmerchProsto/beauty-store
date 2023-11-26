339b6c77498d9e16f584b4e01aaa355f4b87d21a
SmerchProsto <smerchprosto@gmail.com>
2023-11-26 22:32:20 +0400

    update styles for nav
    setted media and styles for it

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const buttonOpen = document.querySelector('.main-nav-btn');
+const nav = document.querySelector('.main-nav');
+const navItems = document.querySelector('.nav-items');
+const buttonSignUp = document.querySelector('.button-signup');
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (window.innerWidth <= 999) {
+        changeMenu()
+    }
+})
+const changeMenu = () => {
+    if (navItems.classList.contains('hidden')) {
+        navItems.classList.remove('hidden');
+    } else if (!navItems.classList.contains('hidden')) {
+        navItems.classList.add('hidden');
+    }
+}
+
+console.log(window.innerWidth);
+
+buttonOpen.addEventListener('click', changeMenu);
+navItems.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') {
+        changeMenu();
+    }
+})
